74ce08cdaa08da7ac485a14eaecbe8ed43a8264a
Gelvazio <gelvazio@unidavi.edu.br>
2020-11-13 23:40:01 -0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # megahack5
 Projeto para o Mega Hack 5
+
+# Passos da instalacao
+na pasta server rodar
+
+npm install admin-bro @admin-bro/express
+
+npm install express express-formidable
+
+# rodar o projeto
+yarn start
+
+# Usuario e senha administrador
+
+Usuario:admin@admin.com
+Senha:admin
---
+++ server/src/server.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const cors = require('cors')
+const express = require("express")
+const server = express()
+
+const admin = require('./admin/router')
+const User = require('./app/controllers/UserController')
+
+server
+  .use(express.json())
+  .use(cors())
+  
+  // routes
+  .use('/admin', admin)
+  .get("/api/users", User.index)
+  .get("/api/users/:id", User.getOne)
+
+
+const run = async() => {
+    const port = 5400;
+    const mongo_url = 'mongodb+srv://admin:megahack5password@cluster0.dfpzp.mongodb.net/megahack5?retryWrites=true&w=majority';
+
+    await mongoose.connect(mongo_url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    server.listen(port, () => console.log("Server started"))
+}
+
+run();
